9045887af8aa7c339ab42109062cc94ba9de283b
Siebrand Mazeland <siebrand@users.mediawiki.org>
2008-01-18 13:10:18 +0000

    Add a few local names

--- LocalNamesEn.php
+++ LocalNamesEn.php
@@ -53,20 +53,48 @@ $names = array(
  */
 'gan'        => 'Gan',
 
-/* Not in CLDR 1.5. Turkic  language.
+/* Not in CLDR 1.5. Aralo-Caspian language.
+ * Added 2008-01-18.
+ */
+'kk-arab'        => 'Kazakh (Arabic script)',
+'kk-cyrl'        => 'Kazakh (Cyrillic script)',
+'kk-latn'        => 'Kazakh (Latin script)',
+
+/* Not in CLDR 1.5. Turkic language.
  * Added 2008-01-14.
  */
 'gag'        => 'Gagauz',
+
+/* Not in CLDR 1.5. Artificial language.
+ * Added 2008-01-18.
+ */
+'lfn'        => 'Lingua Franca Nova',
 
 /* Not in CLDR 1.5. Tibeto-Burman family language.
  * Added 2008-01-14.
  */
 'njo'        => 'Ao Naga',
 
+/* Not in CLDR 1.5. Attic language.
+ * Added 2008-01-18.
+ */
+'pnt'        => 'Pontic',
+
+/* Not in CLDR 1.5. Atlas language.
+ * Added 2008-01-18.
+ */
+'shi'        => 'Tachelhit',
+
 /* Not in CLDR 1.5. Chinese language.
  * Added 2007-12-02.
  */
 'wuu'        => 'Wu',
+
+/* Not in CLDR 1.5. Chinese language.
+ * Added 2008-01-18.
+ */
+'yue'        => 'Yue',
+'zh-yue'        => 'Yue', # language redirect
 
 /* Not in CLDR 1.5. Low Franconian language.
  * Added 2008-01-17.
