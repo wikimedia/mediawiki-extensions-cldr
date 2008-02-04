56bbe72045bc588ab843592050a4a3add251c9c7
Siebrand Mazeland <siebrand@users.mediawiki.org>
2008-02-04 22:57:28 +0000

    Localisation updates for extension messages from Betawiki (2008-02-04 23:35 CET)

--- LanguageNames.i18n.php
+++ LanguageNames.i18n.php
@@ -11,3 +11,18 @@ $messages['en'] = array (
 $messages['fr'] = array (
 	'cldr-desc' => 'Extension qui fournit le nom des langues disponibles',
 );
+
+/** Upper Sorbian (Hornjoserbsce)
+ * @author Michawiki
+ */
+$messages['hsb'] = array(
+	'cldr-desc' => 'Rožšěrjenje, kotrež lokalizowane mjena rěčow k dispoziciji staja.',
+);
+
+/** Dutch (Nederlands)
+ * @author SPQRobin
+ */
+$messages['nl'] = array(
+	'cldr-desc' => 'Uitbreiding die vertaalde namen van talen biedt',
+);
+
