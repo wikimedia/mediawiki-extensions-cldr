c089a122d7c020fa895f4499ddc7e3ab6a5ca80a
Siebrand Mazeland <siebrand@users.mediawiki.org>
2008-08-23 13:39:50 +0000

    Updates of local names and most recent CLDR version

--- LocalNamesEn.php
+++ LocalNamesEn.php
@@ -1,18 +1,18 @@
 <?php
 $names = array(
-/* Does not exist in CLDR 1.5. Name Gheg Albanian used by Ethnologue:
+/* Does not exist in CLDR 1.6.1. Name Gheg Albanian used by Ethnologue:
  * http://www.ethnologue.com/show_language.asp?code=aln
  * Added 2008-01-04.
  */
 'aln'         => 'Gheg Albanian',
 
-/* Does not exist in CLDR 1.5. Arabic language
+/* Does not exist in CLDR 1.6.1. Arabic language
  * http://www.ethnologue.com/show_language.asp?code=arz
  * Added 2008-04-12.
  */
 'arz'         => 'Egyptian Spoken Arabic',
 
-/* Does not exist in CLDR 1.5. Name Bavarian used by Ethnologue:
+/* Does not exist in CLDR 1.6.1. Name Bavarian used by Ethnologue:
  * http://www.ethnologue.com/show_language.asp?code=bar
  * Added 2007-11-10.
  */
@@ -23,103 +23,103 @@ $names = array(
  */
 'bat-smg'     => 'Samogitian',
 
-/* Does not exist in CLDR 1.5. Name Bishnupriya used by Ethnologue:
+/* Does not exist in CLDR 1.6.1. Name Bishnupriya used by Ethnologue:
  * http://www.ethnologue.org/show_language.asp?code=bcc
  * Added 2008-07-02.
  */
 'bcc'         => 'Southern Balochi',
 
-/* Does not exist in CLDR 1.5. Name Bishnupriya used by Ethnologue:
+/* Does not exist in CLDR 1.6.1. Name Bishnupriya used by Ethnologue:
  * http://www.ethnologue.org/show_language.asp?code=bpy
  * Added 2007-11-10.
  */
 'bpy'         => 'Bishnupria Manipuri',
 
-/* Does not exist in CLDR 1.5. Swedish language
+/* Does not exist in CLDR 1.6.1. Swedish language
  * http://www.ethnologue.org/show_language.asp?code=dlc
  * Added 2008-02-16.
  */
 'dlc'         => 'Dalecarlian',
 
-/* Not in CLDR 1.5. Romance language.
+/* Not in CLDR 1.6.1. Romance language.
  * http://www.ethnologue.org/show_language.asp?code=eml
  * Added 2008-01-17.
  */
 'eml'        => 'Emiliano-Romagnolo',
 
-/* Not in CLDR 1.5. Romance language.
+/* Not in CLDR 1.6.1. Romance language.
  * http://www.ethnologue.org/show_language.asp?code=ext
  * Added 2007-12-02.
  */
 'ext'        => 'Extremaduran',
 
-/* Not in CLDR 1.5. Variety of French.
+/* Not in CLDR 1.6.1. Variety of French.
  * http://www.ethnologue.org/show_language.asp?code=frc
  * Added 2007-12-02.
  */
 'frc'        => 'Cajun French',
 
-/* Not in CLDR 1.5. Romance language.
+/* Not in CLDR 1.6.1. Romance language.
  * http://www.ethnologue.org/show_language.asp?code=frp
  * Added 2007-12-02.
  */
 'frp'        => 'Franco-Provençal',
 
-/* Not in CLDR 1.5. Chinese language.
+/* Not in CLDR 1.6.1. Chinese language.
  * http://www.ethnologue.org/show_language.asp?code=hak
  * Added 2007-12-02.
  */
 'hak'        => 'Hakka',
 
-/* Not in CLDR 1.5. Turkic language.
+/* Not in CLDR 1.6.1. Turkic language.
  * Added 2008-01-14.
  */
 'gag'        => 'Gagauz',
 
-/* Not in CLDR 1.5. Chinese language.
+/* Not in CLDR 1.6.1. Chinese language.
  * http://www.ethnologue.org/show_language.asp?code=gan
  * Added 2007-12-02.
  */
 'gan'        => 'Gan',
 
-/* Not in CLDR 1.5. Danish language.
+/* Not in CLDR 1.6.1. Danish language.
  * http://www.ethnologue.org/show_language.asp?code=jut
  * Added 2008-02-06.
  */
 'jut'        => 'Jutish',
 
-/* Not in CLDR 1.5. Aralo-Caspian language.
+/* Not in CLDR 1.6.1. Aralo-Caspian language.
  * Added 2008-01-18.
  */
 'kk-arab'    => 'Kazakh (Arabic script)',
 'kk-cyrl'    => 'Kazakh (Cyrillic script)',
 'kk-latn'    => 'Kazakh (Latin script)',
 
-/* Not in CLDR 1.5. Artificial language.
+/* Not in CLDR 1.6.1. Artificial language.
  * http://www.ethnologue.org/show_language.asp?code=lfn
  * Added 2008-01-18.
  */
 'lfn'        => 'Lingua Franca Nova',
 
-/* Not in CLDR 1.5. Mari language
+/* Not in CLDR 1.6.1. Mari language
  * http://www.ethnologue.org/show_language.asp?code=mhr
  * Added 2008-05-22.
  */
 'mhr'        => 'Eastern Mari',
 
-/* Not in CLDR 1.5. Caspian language
+/* Not in CLDR 1.6.1. Caspian language
  * http://www.ethnologue.org/show_language.asp?code=mzn
  * Added 2008-03-01.
  */
 'mzn'        => 'Mazanderani',
 
-/* Not in CLDR 1.5. Chinese language
+/* Not in CLDR 1.6.1. Chinese language
  * http://www.ethnologue.org/show_language.asp?code=nan
  * Added 2008-03-01.
  */
 'nan'        => 'Min Nan Chinese',
 
-/* Not in CLDR 1.5. Tibeto-Burman family language.
+/* Not in CLDR 1.6.1. Tibeto-Burman family language.
  * http://www.ethnologue.org/show_language.asp?code=njo
  * Added 2008-01-14.
  */
@@ -131,12 +131,12 @@ $names = array(
  */
 'pms'        => 'Piedmontese',
 
-/* Not in CLDR 1.5. Attic language.
+/* Not in CLDR 1.6.1. Attic language.
  * Added 2008-01-18.
  */
 'pnt'        => 'Pontic',
 
-/* Not in CLDR 1.5. Eastern Romance language.
+/* Not in CLDR 1.6.1. Eastern Romance language.
  * http://www.ethnologue.org/show_language.asp?code=ruq
  * Added 2008-02-04.
  */
@@ -145,32 +145,38 @@ $names = array(
 'ruq-grek'   => 'Megleno-Romanian (Greek script)',
 'ruq-latn'   => 'Megleno-Romanian (Latin script)',
 
-/* Not in CLDR 1.5. Atlas language.
+/* Not in CLDR 1.6.1. Atlas language.
  * http://www.ethnologue.org/show_language.asp?code=shi
  * Added 2008-01-18.
  */
 'shi'        => 'Tachelhit',
 
-/* Not in CLDR 1.5.
- * http://www.sil.org/iso639-3/chg_detail.asp?id=2006-106&lang=szl
+/* Not in CLDR 1.6.1.
+ * http://www.sil.org/iso639-3/documentation.asp?id=szl
  * Added 2008-03-29.
  */
 'szl'        => 'Silesian',
 
-/* Not in CLDR 1.5. Chinese language.
+/* Not in CLDR 1.6.1. Tulu language
+ * http://www.sil.org/iso639-3/documentation.asp?id=tcy
+ * Added 2008-08-23.
+ */
+'tcy'        => 'Tulu',
+
+/* Not in CLDR 1.6.1. Chinese language.
  * http://www.ethnologue.org/show_language.asp?code=wuu
  * Added 2007-12-02.
  */
 'wuu'        => 'Wu',
 
-/* Not in CLDR 1.5. Chinese language.
+/* Not in CLDR 1.6.1. Chinese language.
  * http://www.ethnologue.org/show_language.asp?code=que
  * Added 2008-01-18.
  */
 'yue'        => 'Yue',
 'zh-yue'     => 'Yue', # language redirect
 
-/* Not in CLDR 1.5. Low Franconian language.
+/* Not in CLDR 1.6.1. Low Franconian language.
  * http://www.ethnologue.org/show_language.asp?code=zea
  * Added 2008-01-17.
  */
@@ -196,5 +202,6 @@ $names = array(
 'ydd'          => 'Eastern Yiddish',
 'zh-classical' => 'Classical Chinese',
 'zh-hk'        => 'Hong Kong Chinese',
+'zh-sg'        => 'Singapore Chinese',
 'zh-tw'        => 'Taiwan Chinese',
 );
