76f054f3b368b497e7a52a1940e034bf9dad6d0f
Niklas Laxström <nikerabbit@users.mediawiki.org>
2010-03-28 08:34:59 +0000

    Updated info, added documentation

--- LanguageNames.php
+++ LanguageNames.php
@@ -7,14 +7,14 @@ if (!defined('MEDIAWIKI')) die();
  * @file
  *
  * @author Niklas Laxström
- * @copyright Copyright © 2007-2008, Niklas Laxström
+ * @copyright Copyright © 2007-2010, Niklas Laxström
  * @license http://www.gnu.org/copyleft/gpl.html GNU General Public License 2.0 or later
  */
 
 $wgExtensionCredits['other'][] = array(
 	'path' => __FILE__,
 	'name' => 'Language Names',
-	'version' => '1.7.1 (CLDR 1.7.1)',
+	'version' => '1.8.0 (CLDR 1.8.0)',
 	'author' => 'Niklas Laxström',
 	'url' => 'http://unicode.org/cldr/repository_access.html',
 	'descriptionmsg' => 'cldr-desc',
