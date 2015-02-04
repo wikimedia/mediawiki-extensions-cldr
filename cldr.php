67d7aa3e5052592e28f564004f1a5c3a0385d4a4
Adrian Heine <adrian.heine@wikimedia.de>
2015-02-04 09:26:17 +0100

    Explicitly reference globals
    This is necessary for loading Cldr via composer.
    
    Change-Id: I29f56149ef51aa988c5f503bc3cc621a8f0c023a

--- cldr.php
+++ cldr.php
@@ -11,7 +11,7 @@ if ( !defined( 'MEDIAWIKI' ) ) die();
  */
 define( 'CLDR_VERSION', '4.1.0 (CLDR 26)' );
 
-$wgExtensionCredits['other'][] = array(
+$GLOBALS['wgExtensionCredits']['other'][] = array(
 	'path' => __FILE__,
 	'name' => 'Language Names',
 	'version' => CLDR_VERSION,
@@ -21,12 +21,12 @@ $wgExtensionCredits['other'][] = array(
 );
 
 $dir = dirname( __FILE__ ) . '/';
-$wgMessagesDirs['cldr'] = __DIR__ . '/i18n';
-$wgExtensionMessagesFiles['cldr'] = $dir . 'cldr.i18n.php';
-$wgAutoloadClasses['CldrNames'] = $dir . 'CldrNames.php';
-$wgAutoloadClasses['LanguageNames'] = $dir . 'LanguageNames.body.php';
-$wgAutoloadClasses['CountryNames'] = $dir . 'CountryNames.body.php';
-$wgAutoloadClasses['CurrencyNames'] = $dir . 'CurrencyNames.body.php';
-$wgAutoloadClasses['TimeUnits'] = $dir . 'TimeUnits.body.php';
-$wgHooks['LanguageGetTranslatedLanguageNames'][] = 'LanguageNames::coreHook';
-$wgHooks['GetHumanTimestamp'][] = 'TimeUnits::onGetHumanTimestamp';
+$GLOBALS['wgMessagesDirs']['cldr'] = __DIR__ . '/i18n';
+$GLOBALS['wgExtensionMessagesFiles']['cldr'] = $dir . 'cldr.i18n.php';
+$GLOBALS['wgAutoloadClasses']['CldrNames'] = $dir . 'CldrNames.php';
+$GLOBALS['wgAutoloadClasses']['LanguageNames'] = $dir . 'LanguageNames.body.php';
+$GLOBALS['wgAutoloadClasses']['CountryNames'] = $dir . 'CountryNames.body.php';
+$GLOBALS['wgAutoloadClasses']['CurrencyNames'] = $dir . 'CurrencyNames.body.php';
+$GLOBALS['wgAutoloadClasses']['TimeUnits'] = $dir . 'TimeUnits.body.php';
+$GLOBALS['wgHooks']['LanguageGetTranslatedLanguageNames'][] = 'LanguageNames::coreHook';
+$GLOBALS['wgHooks']['GetHumanTimestamp'][] = 'TimeUnits::onGetHumanTimestamp';
