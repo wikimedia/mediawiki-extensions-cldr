7d110e020e0e6d60f9e7c8b402a15f072c6d5336
Raymond <raimond.spekking@gmail.com>
2017-02-04 19:35:37 +0100

    Add LocalNames for German
    Only for languages which do not have an entry in CLDR but have a Wikipedia
    in this language
    
    Change-Id: I256d68bc2f7a218303b3706868d553d84b497d92

---
+++ LocalNames/LocalNamesDe.php
@@ -0,0 +1,19 @@
+<?php
+
+$languageNames = [
+
+	/* Not in CLDR30. But Wikipedias in these languages exist */
+	'bat-smg' => 'Schemaitisch',
+	'be-tarask' => 'Weißrussisch (Taraschkewiza)',
+	'cdo' => 'Min Dong',
+	'crh' => 'Krimtatarisch',
+	'eml' => 'Emilianisch',
+	'hif' => 'Fidschi-Hindi',
+	'mhr' => 'Ostmari',
+	'olo' => 'Olonetzisch',
+	'pnb' => 'West-Panjabi',
+	'roa-rup' => 'Aromunisch',
+	'zh-classical' => 'klassisches Chinesisch',
+	'zh-min-nan' => 'Chinesisch (Min Nan)',
+	'zh-yue' => 'Kantonesisch',
+];
