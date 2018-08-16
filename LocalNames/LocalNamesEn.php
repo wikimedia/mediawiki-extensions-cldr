91be27a635d39b5c720d787463a983d11b71235e
Raymond <raimond.spekking@gmail.com>
2018-08-16 08:52:22 +0200

    Sort languages by code
    I think it is unimportant since when a code is not in CLDR.
    With every new CLDR release a check should be done
    
    Change-Id: I13999ee7bdaa56be6db971b46ff7bbaa45d41742

--- LocalNames/LocalNamesEn.php
+++ LocalNames/LocalNamesEn.php
@@ -1,127 +1,7 @@
 <?php
 
 $languageNames = [
-
-	/* Not in CLDR 27.0.1. Balochi language
-	 * https://www.ethnologue.com/language/bcc
-	 * Added 2008-07-02.
-	 */
-	'bcc' => 'Southern Balochi',
-
-	/* Not in CLDR 27.0.1. Eastern Balochi language
-	 * http://www.ethnologue.com/language/bgp
-	 */
-	'bgp' => 'Eastern Balochi',
-
-	/* Not in CLDR 27.0.1. Western Cham language
-	* https://www.ethnologue.com/language/cja
-	*/
-	'cja' => 'Western Cham',
-	'cja-cham' => 'Western Cham (Cham script)',
-	'cja-latn' => 'Western Cham (Latin script)',
-	'cja-arab' => 'Western Cham (Arabic script)',
-
-	/* Not in CLDR 27.0.1. Eastern Cham language
-	* https://www.ethnologue.com/language/cjm
-	*/
-	'cjm' => 'Eastern Cham',
-	'cjm-cham' => 'Eastern Cham (Cham script)',
-	'cjm-latn' => 'Eastern Cham (Latin script)',
-	'cjm-arab' => 'Eastern Cham (Arabic script)',
-
-	/* Not in CLDR 27.0.1. Jinyu language
-	 * https://www.ethnologue.com/language/cjy
-	 */
-	'cjy' => 'Jinyu',
-	'cjy-hans' => 'Jinyu (Simplified)',
-	'cjy-hant' => 'Jinyu (Traditional)',
-
-	/* Not in CLDR 32.0.1 Montenegrin language
-	 * https://www.ethnologue.com/language/cnr
-	 */
-	'cnr' => 'Montenegrin',
-
-	/* Not in CLDR 27.0.1. Swedish language
-	 * https://www.ethnologue.com/language/dlc
-	 * Added 2008-02-16.
-	 */
-	'dlc' => 'Dalecarlian',
-
-	/* Not in CLDR 27.0.1. Romance language.
-	 * https://www.ethnologue.com/language/eml
-	 * Added 2008-01-17.
-	 */
-	'eml' => 'Emiliano-Romagnolo',
-
-	/* Not in CLDR 27.0.1. Konkani language
-	 * https://www.ethnologue.com/language/gom
-	 * Added 2008-09-02.
-	 */
-	'gom-deva' => 'Goan Konkani (Devanagari script)',
-	'gom-latn' => 'Goan Konkani (Latin script)',
-
-	/* Not in CLDR 27.0.1. Maharashtrian Konkani language
-	 * https://www.ethnologue.com/language/knn
-	 */
-	'knn' => 'Maharashtrian Konkani',
-
-	/* Not in CLDR 27.0.1. Laki language
-	 * https://www.ethnologue.com/language/lki
-	 */
-	'lki' => 'Laki',
-
-	/* Not in CLDR 27.0.1. Southern Luri language
-	 * https://www.ethnologue.com/language/luz
-	 */
-	'luz' => 'Southern Luri',
-
-	/* Not in CLDR 27.0.1. Mari language
-	 * https://www.ethnologue.com/language/mhr
-	 * Added 2008-05-22.
-	 */
-	'mhr' => 'Eastern Mari',
-
-	/* Not in CLDR 27.0.1. Malay language
-	 * https://www.ethnologue.com/language/mui
-	 * Added 2010-02-11.
-	 */
-	'mui' => 'Musi',
-
-	/* Not in CLDR 27.0.1. Livvi-Karelian language
-	 * https://www.ethnologue.com/language/olo
-	 */
-	'olo' => 'Livvi-Karelian',
-
-	/* Not in CLDR 27.0.1. Lahnda language.
-	 * https://www.ethnologue.com/language/pnb
-	 * Added 2008-10-29.
-	 */
-	'pnb' => 'Western Punjabi',
-
-	/* Not in CLDR 27.0.1. Dari language.
-	 * https://www.ethnologue.com/language/prs
-	 */
-	'prs' => 'Dari',
-
-	/* Not in CLDR 27.0.1. Eastern Romance language.
-	 * https://www.ethnologue.com/language/ruq
-	 * Added 2008-02-04.
-	 */
-	'ruq' => 'Megleno-Romanian',
-	'ruq-cyrl' => 'Megleno-Romanian (Cyrillic script)',
-	'ruq-grek' => 'Megleno-Romanian (Greek script)',
-	'ruq-latn' => 'Megleno-Romanian (Latin script)',
-
-	/* Not in CLDR 27.0.1. Shawiya language
-	 * https://www.ethnologue.com/language/shy
-	 */
-	'shy' => 'Shawiya',
-	'shy-arab' => 'Shawiya (Arabic script)',
-	'shy-latn' => 'Shawiya (Latin script)',
-	'shy-tfng' => 'Shawiya (Tifinagh script)',
-
-	/* Not in CLDR 27.0.1. Used for MediaWiki localisation
-	 */
+	# Not in CLDR 33.1. Used for MediaWiki localisation
 	'abe' => 'Western Abenaki',
 	'abs' => 'Ambonese Malay',
 	'ady-cyrl' => 'Adyghe (Cyrillic script)',
@@ -131,18 +11,34 @@ $languageNames = [
 	'azb' => 'South Azerbaijani',
 	'bat-smg' => 'Samogitian',
 	'bbc-latn' => 'Batak Toba (Latin script)',
+	'bcc' => 'Southern Balochi',
 	'bcl' => 'Central Bikol',
 	'be-tarask' => 'Belarusian (Taraškievica orthography)',
 	'be-x-old' => 'Belarusian (Taraškievica orthography)', # internal code of be-tarask.wikipedia.org
+	'bgp' => 'Eastern Balochi',
 	'bh' => 'Bhojpuri',
 	'bnn' => 'Bunun',
 	'bxr' => 'Russia Buriat',
 	'cbk-zam' => 'Chavacano',
 	'cdo' => 'Min Dong Chinese',
+	'cja' => 'Western Cham',
+	'cja-cham' => 'Western Cham (Cham script)',
+	'cja-latn' => 'Western Cham (Latin script)',
+	'cja-arab' => 'Western Cham (Arabic script)',
+	'cjm' => 'Eastern Cham',
+	'cjm-cham' => 'Eastern Cham (Cham script)',
+	'cjm-latn' => 'Eastern Cham (Latin script)',
+	'cjm-arab' => 'Eastern Cham (Arabic script)',
+	'cjy' => 'Jinyu',
+	'cjy-hans' => 'Jinyu (Simplified)',
+	'cjy-hant' => 'Jinyu (Traditional)',
+	'cnr' => 'Montenegrin',
 	'crh-cyrl' => 'Crimean Tatar (Cyrillic script)',
 	'crh-latn' => 'Crimean Tatar (Latin script)',
 	'de-formal' => 'German (formal address)',
+	'dlc' => 'Dalecarlian',
 	'dty' => 'Doteli',
+	'eml' => 'Emiliano-Romagnolo',
 	'ett' => 'Etruscian',
 	'eya' => 'Eyak',
 	'fkv' => 'Kvensk',
@@ -150,6 +46,8 @@ $languageNames = [
 	'fuf' => 'Pular',
 	'gan-hans' => 'Gan (Simplified)',
 	'gan-hant' => 'Gan (Traditional)',
+	'gom-deva' => 'Goan Konkani (Devanagari script)',
+	'gom-latn' => 'Goan Konkani (Latin script)',
 	'hif-latn' => 'Fiji Hindi (Latin script)',
 	'hyw' => 'Western Armenian',
 	'ike-cans' => 'Eastern Canadian (Aboriginal syllabics)',
@@ -163,6 +61,7 @@ $languageNames = [
 	'kk-arab' => 'Kazakh (Arabic script)',
 	'kk-cyrl' => 'Kazakh (Cyrillic script)',
 	'kk-latn' => 'Kazakh (Latin script)',
+	'knn' => 'Maharashtrian Konkani',
 	'ko-kp' => 'Korean (North Korea)',
 	'koy' => 'Koyukon',
 	'ks-arab' => 'Kashmiri (Arabic script)',
@@ -170,27 +69,40 @@ $languageNames = [
 	'ku-arab' => 'Kurdish (Arabic script)',
 	'ku-latn' => 'Kurdish (Latin script)',
 	'lbe' => 'Lak',
+	'lki' => 'Laki',
 	'lld' => 'Ladin',
+	'luz' => 'Southern Luri',
+	'mhr' => 'Eastern Mari',
+	'mis' => 'Unsupported language',
 	'mo' => 'Moldovan',
 	'moe' => 'Innu',
-	'mis' => 'Unsupported language',
 	'mnw' => 'Mon',
+	'mui' => 'Musi',
 	'nod' => 'Northern Thai',
 	'nrm' => 'Norman',
 	'nxm' => 'Numidian',
+	'olo' => 'Livvi-Karelian',
 	'ood' => "O'odham",
 	'otk' => 'Old Turkish',
 	'pjt' => 'Pitjantjatjara',
+	'pnb' => 'Western Punjabi',
 	'ppu' => 'Papora-Hoanya',
 	'pwn' => 'Paiwan',
 	'pyu' => 'Puyuma',
+	'prs' => 'Dari',
 	'roa-rup' => 'Aromanian',
 	'roa-tara' => 'Tarantino',
+	'ruq' => 'Megleno-Romanian',
+	'ruq-cyrl' => 'Megleno-Romanian (Cyrillic script)',
+	'ruq-grek' => 'Megleno-Romanian (Greek script)',
+	'ruq-latn' => 'Megleno-Romanian (Latin script)',
 	'rwr' => 'Marwari (India)',
 	'shi-latn' => 'Tachelhit (Latin script)',
 	'shi-tfng' => 'Tachelhit (Tifinagh script)',
 	'shy' => 'Shawiya',
+	'shy-arab' => 'Shawiya (Arabic script)',
 	'shy-latn' => 'Shawiya (Latin script)',
+	'shy-tfng' => 'Shawiya (Tifinagh script)',
 	'sjd' => 'Kildin Sami',
 	'sje' => 'Pite Sami',
 	'sju' => 'Ume Sami',
