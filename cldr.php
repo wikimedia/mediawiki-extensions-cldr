9b1ecf32a8d29ff571b3a8254648968301c3b8fb
Umherirrender <umherirrender_de.wp@web.de>
2018-10-26 20:49:14 +0200

    Start showing warning for deprecated PHP entry point
    Change-Id: I1f89b36a12e5b5207a8b38f7b17676841c04fe32

--- cldr.php
+++ cldr.php
@@ -11,10 +11,10 @@ if ( function_exists( 'wfLoadExtension' ) ) {
 	wfLoadExtension( 'cldr' );
 	// Keep i18n globals so mergeMessageFileList.php doesn't break
 	$wgMessagesDirs['cldr'] = __DIR__ . '/i18n';
-	/*wfWarn(
+	wfWarn(
 		'Deprecated PHP entry point used for cldr extension. Please use wfLoadExtension instead, ' .
 		'see https://www.mediawiki.org/wiki/Extension_registration for more details.'
-	);*/
+	);
 	return;
 } else {
 	die( 'This version of the cldr extension requires MediaWiki 1.25+' );
