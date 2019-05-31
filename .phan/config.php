920fcaea13b19169ce604ec39fa5f47cbd851bf5
Umherirrender <umherirrender_de.wp@web.de>
2019-05-31 16:54:39 +0200

    Add phan
    Change-Id: I189d206484076245aa35da3cdf5623e1f19cb3f8

---
+++ .phan/config.php
@@ -0,0 +1,18 @@
+<?php
+
+$cfg = require __DIR__ . '/../vendor/mediawiki/mediawiki-phan-config/src/config.php';
+
+$cfg['file_list'][] = 'CldrCurrencies.php';
+$cfg['file_list'][] = 'rebuild.php';
+
+$cfg['directory_list'] = array_merge(
+	$cfg['directory_list'],
+	[
+		'CldrCurrency/',
+		'CldrNames/',
+		'CldrSupplemental/',
+		'LocalNames/',
+	]
+);
+
+return $cfg;
