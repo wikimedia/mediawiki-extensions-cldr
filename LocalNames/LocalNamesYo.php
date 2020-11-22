efc18c16160f6cd259285d0e17fe8a7555a0b0ee
Raymond <raimond.spekking@gmail.com>
2020-11-22 13:16:38 +0100

    Fix missing bracket
    Reported upstream: https://unicode-org.atlassian.net/browse/CLDR-14280
    
    Bug: T267265
    Change-Id: I4725eaf1699b00b227d822a34278ba28f9790685

---
+++ LocalNames/LocalNamesYo.php
@@ -0,0 +1,11 @@
+<?php
+
+$languageNames = [
+
+	/** Not in CLDR.
+	 * But Wikipedias in these languages exist and/or used by Wikidata for monolingual text
+	 */
+
+	# Reported upstream: https://unicode-org.atlassian.net/browse/CLDR-14280
+	'es-419' => 'Èdè Sípáníìṣì (orílẹ̀-èdè Látìn-Amẹ́ríkà) ( Èdè Sípáníìshì (Látìn-Amẹ́ríkà))',
+];
