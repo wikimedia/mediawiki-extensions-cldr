8aecee665fd1941bbc6a5b2dfb0bc3f526ed84bf
Raymond <raimond.spekking@gmail.com>
2022-05-09 16:55:39 +0200

    Add autonym for Hadza
    Bug: T303379
    Change-Id: I9e787f9f8e5210ad85fd3385038cb2e8a7cdc9b3

---
+++ LocalNames/LocalNamesHts.php
@@ -0,0 +1,10 @@
+<?php
+
+/** @phpcs-require-sorted-array */
+$languageNames = [
+
+	/** Not in CLDR.
+	 * But Wikipedias in these languages exist and/or used by Wikidata for monolingual text
+	 */
+	'hts' => 'Haza',
+];
