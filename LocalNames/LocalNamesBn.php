e0c568f35c7d72fab3866e73daea8378470566c3
Bartosz Dziewoński <matma.rex@gmail.com>
2022-09-01 02:26:30 +0200

    Override localisation for "hour" in 'bn'
    https://unicode-org.atlassian.net/browse/CLDR-15975
    
    It looks like we haven't tried to override $timeUnits before,
    but it's supported.
    
    Bug: T316843
    Change-Id: Ie0ad876e2d724f740eb81d5d2ae325cd146c4b52

---
+++ LocalNames/LocalNamesBn.php
@@ -0,0 +1,21 @@
+<?php
+
+// Correct translation of "hour" to be consistent with MediaWiki (T316843)
+// Proposed in CLDR: https://unicode-org.atlassian.net/browse/CLDR-15975
+// TODO: Remove when fixed upstream
+$timeUnits = [
+	'hour-future-one' => '{0} ঘণ্টায়',
+	'hour-future-other' => '{0} ঘণ্টায়',
+	'hour-narrow-future-one' => '{0} ঘণ্টায়',
+	'hour-narrow-future-other' => '{0} ঘণ্টায়',
+	'hour-narrow-past-one' => '{0} ঘণ্টা আগে',
+	'hour-narrow-past-other' => '{0} ঘণ্টা আগে',
+	'hour-one' => '{0} ঘণ্টা',
+	'hour-other' => '{0} ঘণ্টা',
+	'hour-past-one' => '{0} ঘণ্টা আগে',
+	'hour-past-other' => '{0} ঘণ্টা আগে',
+	'hour-short-future-one' => '{0} ঘণ্টায়',
+	'hour-short-future-other' => '{0} ঘণ্টায়',
+	'hour-short-past-one' => '{0} ঘণ্টা আগে',
+	'hour-short-past-other' => '{0} ঘণ্টা আগে',
+];
