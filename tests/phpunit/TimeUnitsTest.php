d665c4d36d7f6102ddcd51c91894b87a841d7245
Umherirrender <umherirrender_de.wp@web.de>
2022-12-28 19:36:32 +0100

    tests: Replace deprecated Language::factory
    Bug: T325986
    Change-Id: I1ff4b0505efa7c86615f93de3f4620a458e085f9

--- tests/phpunit/TimeUnitsTest.php
+++ tests/phpunit/TimeUnitsTest.php
@@ -1,4 +1,7 @@
 <?php
+
+use MediaWiki\MediaWikiServices;
+
 /**
  * Tests for TimeUnits
  * @author Santhosh Thottingal
@@ -18,7 +21,7 @@ class TimeUnitsTest extends MediaWikiIntegrationTestCase {
 	 * @param string $desc Description
 	 */
 	public function testTimeUnit( $language, $tsTime, $currentTime, $expectedOutput, $desc ) {
-		$lang = Language::factory( $language );
+		$lang = MediaWikiServices::getInstance()->getLanguageFactory()->getLanguage( $language );
 		$tsTime = new MWTimestamp( $tsTime );
 		$currentTime = new MWTimestamp( $currentTime );
 		$this->assertEquals(
