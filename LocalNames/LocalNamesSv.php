fcc8cbb98cd298828775331cf7c408ef629a7753
Raymond <raimond.spekking@gmail.com>
2023-07-15 19:39:49 +0200

    Add language names for "be" and "be-tarask" for Swedish (sv)
    Bug: T341917
    Change-Id: Iecfc687781b7aed997b0b435eb228a19e27830a6

--- LocalNames/LocalNamesSv.php
+++ LocalNames/LocalNamesSv.php
@@ -2,6 +2,9 @@
 
 /** @phpcs-require-sorted-array */
 $languageNames = [
+	# Override CLDR name, see T341917/CLDR-16876
+	'be' => 'belarusiska',
+	'be-tarask' => 'belarusiska (tarasjkevitsa)',
 	# Override CLDR name, see T281702
 	'ps' => 'pashto',
 ];
