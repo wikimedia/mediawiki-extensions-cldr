189d424807ad8a42d9f93b5505298403a1177963
Nikki <randomemail2468@protonmail.com>
2024-01-05 11:26:40 +0000

    Import language names for fy and tok from WikibaseLexeme
    The WikibaseLexeme repository contains some language names which have
    been translated via Translatewiki. Adding them here will make the names
    available elsewhere in MediaWiki and will stop the names from being
    lost when it is updated to fetch names from the CLDR extension instead.
    
    Author: Robin van der Vliet
    Source: https://gerrit.wikimedia.org/r/plugins/gitiles/mediawiki/extensions/WikibaseLexeme/+/56332583e9412965e890ec896202a678f22c9e00/i18n/language-names/fy.json
    Source: https://gerrit.wikimedia.org/r/plugins/gitiles/mediawiki/extensions/WikibaseLexeme/+/56332583e9412965e890ec896202a678f22c9e00/i18n/language-names/tok.json
    
    Change-Id: Ia85e3ae8a83800415d2ff2b53543b13bb2b56871

---
+++ LocalNames/LocalNamesFy.php
@@ -0,0 +1,9 @@
+<?php
+
+/** @phpcs-require-sorted-array */
+$languageNames = [
+	'bfi' => 'Britske Gebeartetaal',
+	'eo-hsistemo' => 'Esperanto (h-systeem)',
+	'eo-xsistemo' => 'Esperanto (x-systeem)',
+	'gsg' => 'Dútske Gebeartetaal',
+];
---
+++ LocalNames/LocalNamesTok.php
@@ -0,0 +1,7 @@
+<?php
+
+/** @phpcs-require-sorted-array */
+$languageNames = [
+	'bfi' => 'toki luka Juke',
+	'gsg' => 'toki luka Tosi',
+];
