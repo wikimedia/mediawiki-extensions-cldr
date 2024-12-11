e49c1951b5f7adbc41d777b4dc748bd3b6054506
Arthur Taylor <arthur.taylor@wikimedia.de>
2024-12-11 13:29:07 +0100

    Import language names for id from WikibaseLexeme
    The WikibaseLexeme repository contains some language names which have
    been translated via Translatewiki. Adding them here will make the names
    available elsewhere in MediaWiki and will stop the names from being
    lost when it is updated to fetch names from the CLDR extension instead.
    
    Author: Daud I.F. Argana
    Author: Es Krim 5 Juta Rasa
    Author: Veracious
    Source: https://gerrit.wikimedia.org/r/plugins/gitiles/mediawiki/extensions/WikibaseLexeme/+/c0d0a6f9559c115ea06e5f10bcdaa440aa5b2f3d/i18n/language-names/id.json
    Bug: T352922
    Change-Id: If9849d05d1571beb013f52f0d5c128214b5692b8

--- LocalNames/LocalNamesId.php
+++ LocalNames/LocalNamesId.php
@@ -3,4 +3,10 @@
 /** @phpcs-require-sorted-array */
 $languageNames = [
 	'btd' => 'Batak Pakpak',
+	'bzs' => 'Bahasa Isyarat Brasil',
+	'de-1901' => 'Jerman (ortografi tradisional)',
+	'hoc' => 'Ho',
+	'mis' => 'lainnya',
+	'sat-latn' => 'Santali (aksara Latin)',
+	'sux-xsux' => 'Sumeria (aksara Sumeria)',
 ];
