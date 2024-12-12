2727b0e493b9f32f2c7b4f575ba10819fea1d108
Arthur Taylor <arthur.taylor@wikimedia.de>
2024-12-12 10:54:15 +0100

    Import language names for cy from WikibaseLexeme
    The WikibaseLexeme repository contains some language names which have
    been translated via Translatewiki. Adding them here will make the names
    available elsewhere in MediaWiki and will stop the names from being
    lost when it is updated to fetch names from the CLDR extension instead.
    
    Author: Afalau
    Author: Ceirios
    Author: Robin Owain
    Source: https://gerrit.wikimedia.org/r/plugins/gitiles/mediawiki/extensions/WikibaseLexeme/+/c0d0a6f9559c115ea06e5f10bcdaa440aa5b2f3d/i18n/language-names/cy.json
    Bug: T352922
    Change-Id: I6dd479f7fc92ce3a4f29c110e2369f531bb4e45a

---
+++ LocalNames/LocalNamesCy.php
@@ -0,0 +1,18 @@
+<?php
+
+/** @phpcs-require-sorted-array */
+$languageNames = [
+	'az-cyrl' => 'Aserbaijaneg mewn sgript Cyrilig',
+	'bfi' => 'Iaith Arwyddion Prydain',
+	'bzs' => 'Iaith Arwyddion Brasil',
+	'cak' => 'Kaqchikel',
+	'de-1901' => 'Almaeneg (cyn diwygiad 1996)',
+	'eo-hsistemo' => 'Esperanto (system h)',
+	'eo-xsistemo' => 'Esperanto (system x)',
+	'gsg' => 'Iaith Arwyddion Almaeneg',
+	'hoc' => 'Ho',
+	'mis' => 'arall',
+	'nrf-je' => 'Jèrriais',
+	'pt-ao1990' => 'Portiwgaleg (Cytundeb Orthograffig Portiwgaleg 1990)',
+	'sat-latn' => 'Santali mewn sgript Lladin',
+];
