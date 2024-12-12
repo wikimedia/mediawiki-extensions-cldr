dfcba05899e3bfb395b9810a1948296807953af6
Arthur Taylor <arthur.taylor@wikimedia.de>
2024-12-12 13:28:44 +0100

    Import language names for ojb from WikibaseLexeme
    The WikibaseLexeme repository contains some language names which have
    been translated via Translatewiki. Adding them here will make the names
    available elsewhere in MediaWiki and will stop the names from being
    lost when it is updated to fetch names from the CLDR extension instead.
    
    Source: https://gerrit.wikimedia.org/r/plugins/gitiles/mediawiki/extensions/WikibaseLexeme/+/c0d0a6f9559c115ea06e5f10bcdaa440aa5b2f3d/i18n/language-names/ojb.json
    Bug: T352922
    Change-Id: If00bbcbb722e268775d01fcc24591146a1334a6e

---
+++ LocalNames/LocalNamesOjb.php
@@ -0,0 +1,8 @@
+<?php
+
+/** @phpcs-require-sorted-array */
+$languageNames = [
+	'az-cyrl' => 'Aserbaijani ozhibii’an wiji-odaminwaagan-mitigowan-aki ozhibii’igan',
+	'ctg' => 'Agwaataa Chittagon',
+	'rkt' => 'Agwaataa Angpo',
+];
