19c783e62466361181bad2f18c84a2f1b109c3bc
Arthur Taylor <arthur.taylor@wikimedia.de>
2024-12-12 13:23:25 +0100

    Import language names for guw from WikibaseLexeme
    The WikibaseLexeme repository contains some language names which have
    been translated via Translatewiki. Adding them here will make the names
    available elsewhere in MediaWiki and will stop the names from being
    lost when it is updated to fetch names from the CLDR extension instead.
    
    Author: Anthonymodec
    Source: https://gerrit.wikimedia.org/r/plugins/gitiles/mediawiki/extensions/WikibaseLexeme/+/c0d0a6f9559c115ea06e5f10bcdaa440aa5b2f3d/i18n/language-names/guw.json
    Bug: T352922
    Change-Id: If6949fef0fd5f81bc248ff6549dbfbe1affc4bf8

---
+++ LocalNames/LocalNamesGuw.php
@@ -0,0 +1,13 @@
+<?php
+
+/** @phpcs-require-sorted-array */
+$languageNames = [
+	'bfi' => 'Aloyidodoho British ton',
+	'enm' => 'Agbedemeji geesi',
+	'gsg' => 'Aloyidodoho German ton',
+	'ha-arab' => 'Akosile hausa ni arab',
+	'sux-latn' => 'Kandai Sumerian to latin',
+	'sux-xsux' => 'Sumerian to Sumerian kandai me',
+	'tlh-latn' => 'Akosile Klingon ni latin',
+	'tlh-piqd' => 'Kandai klingon to Klingon',
+];
