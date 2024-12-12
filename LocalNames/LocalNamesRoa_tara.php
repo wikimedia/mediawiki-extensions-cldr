2088f43ca43761af27af61e7c9507288c6ca40cb
Arthur Taylor <arthur.taylor@wikimedia.de>
2024-12-12 13:29:38 +0100

    Import language names for roa-tara from WikibaseLexeme
    The WikibaseLexeme repository contains some language names which have
    been translated via Translatewiki. Adding them here will make the names
    available elsewhere in MediaWiki and will stop the names from being
    lost when it is updated to fetch names from the CLDR extension instead.
    
    Author: Joetaras
    Source: https://gerrit.wikimedia.org/r/plugins/gitiles/mediawiki/extensions/WikibaseLexeme/+/c0d0a6f9559c115ea06e5f10bcdaa440aa5b2f3d/i18n/language-names/roa-tara.json
    Bug: T352922
    Change-Id: I511eb39692b3fbbfca7149d89e271cd868586b84

---
+++ LocalNames/LocalNamesRoa_tara.php
@@ -0,0 +1,10 @@
+<?php
+
+/** @phpcs-require-sorted-array */
+$languageNames = [
+	'rm-puter' => 'Puter',
+	'rm-surmiran' => 'Surmirane',
+	'rm-sursilv' => 'Sursilvane',
+	'rm-sutsilv' => 'Sutsilvane',
+	'rm-vallader' => 'Vallader',
+];
