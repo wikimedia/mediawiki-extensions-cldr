c3017d4d71cd44fdb2b9998e8f324432f6197322
Arthur Taylor <arthur.taylor@wikimedia.de>
2024-12-12 10:49:36 +0100

    Import language names for ca from WikibaseLexeme
    The WikibaseLexeme repository contains some language names which have
    been translated via Translatewiki. Adding them here will make the names
    available elsewhere in MediaWiki and will stop the names from being
    lost when it is updated to fetch names from the CLDR extension instead.
    
    Author: Lluis Cat
    Author: Mguix
    Author: Toniher
    Source: https://gerrit.wikimedia.org/r/plugins/gitiles/mediawiki/extensions/WikibaseLexeme/+/c0d0a6f9559c115ea06e5f10bcdaa440aa5b2f3d/i18n/language-names/ca.json
    Bug: T352922
    Change-Id: I3649bb118f0258c0462d5c0786c8f003991f9f1b

---
+++ LocalNames/LocalNamesCa.php
@@ -0,0 +1,10 @@
+<?php
+
+/** @phpcs-require-sorted-array */
+$languageNames = [
+	'de-1901' => 'Alemany (abans de la reforma del 1996)',
+	'lij-mc' => 'Monegasc',
+	'mis' => 'llengua no compatible',
+	'non-runr' => 'Nòrdic antic (escriptura rúnica)',
+	'rah' => 'Rabha',
+];
