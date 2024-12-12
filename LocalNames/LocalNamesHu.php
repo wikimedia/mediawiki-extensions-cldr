074b33faac90bfe04c6b870695f70dae6589ce00
Arthur Taylor <arthur.taylor@wikimedia.de>
2024-12-12 11:12:01 +0100

    Import language names for hu from WikibaseLexeme
    The WikibaseLexeme repository contains some language names which have
    been translated via Translatewiki. Adding them here will make the names
    available elsewhere in MediaWiki and will stop the names from being
    lost when it is updated to fetch names from the CLDR extension instead.
    
    Author: Hanna Tardos
    Author: Misibacsi
    Source: https://gerrit.wikimedia.org/r/plugins/gitiles/mediawiki/extensions/WikibaseLexeme/+/c0d0a6f9559c115ea06e5f10bcdaa440aa5b2f3d/i18n/language-names/hu.json
    Bug: T352922
    Change-Id: I8631d9e44e6c1e35c139228e970d0bbf8177844c

---
+++ LocalNames/LocalNamesHu.php
@@ -0,0 +1,18 @@
+<?php
+
+/** @phpcs-require-sorted-array */
+$languageNames = [
+	'bzs' => 'brazil jelnyelv',
+	'de-1901' => 'Német (1996-os reform előtti)',
+	'eo-hsistemo' => 'Eszperantó (h-rendszer)',
+	'eo-xsistemo' => 'Eszperantó (x-rendszer)',
+	'ja-hira' => 'Hiragana japán',
+	'ja-hrkt' => 'Hiragana és katana japán',
+	'ja-kana' => 'Katakana japán',
+	'lij-mc' => 'monacói',
+	'obt' => 'óbreton',
+	'pks' => 'Pakisztáni jelnyelv',
+	'sia' => 'akkala számi',
+	'sjk' => 'kemi számi',
+	'xbm' => 'középbreton',
+];
