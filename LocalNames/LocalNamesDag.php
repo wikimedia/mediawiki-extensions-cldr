41825e8ea5bb5c29595e1b148324a143cd0056ae
Arthur Taylor <arthur.taylor@wikimedia.de>
2024-12-12 13:22:28 +0100

    Import language names for dag from WikibaseLexeme
    The WikibaseLexeme repository contains some language names which have
    been translated via Translatewiki. Adding them here will make the names
    available elsewhere in MediaWiki and will stop the names from being
    lost when it is updated to fetch names from the CLDR extension instead.
    
    Author: Alhassan Mohammed Awal
    Author: Alhassan Peter
    Author: Hasslaebetch
    Author: Issahiddris98
    Author: Musahfm
    Author: Sir Amugi
    Source: https://gerrit.wikimedia.org/r/plugins/gitiles/mediawiki/extensions/WikibaseLexeme/+/c0d0a6f9559c115ea06e5f10bcdaa440aa5b2f3d/i18n/language-names/dag.json
    Bug: T352922
    Change-Id: Id0c1510c49e30236da17d4bb8d4d366a68a71d6d

---
+++ LocalNames/LocalNamesDag.php
@@ -0,0 +1,16 @@
+<?php
+
+/** @phpcs-require-sorted-array */
+$languageNames = [
+	'bas' => 'Basaa',
+	'cak' => 'Kaqchikel',
+	'cnh' => 'Hakha-Chini',
+	'fon' => 'Fon',
+	'gmh' => 'Sunsuuni dubu German',
+	'goh' => 'Germani kurili',
+	'ja-hira' => 'Japanese (Hiragana script)',
+	'nn-hognorsk' => 'Norwegian Høgnorsk',
+	'pt-ao1990' => 'Portuguesi (19990 orthographic agreement)',
+	'pt-colb1945' => 'Portuguesi(1945 orthographic agreement)',
+	'quc' => 'Kʼicheʼ',
+];
