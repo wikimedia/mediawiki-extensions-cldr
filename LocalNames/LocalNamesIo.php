86e7ae0afd3e2b13198a288f633cfcde2cc7629f
Arthur Taylor <arthur.taylor@wikimedia.de>
2024-12-12 11:21:09 +0100

    Import language names for io from WikibaseLexeme
    The WikibaseLexeme repository contains some language names which have
    been translated via Translatewiki. Adding them here will make the names
    available elsewhere in MediaWiki and will stop the names from being
    lost when it is updated to fetch names from the CLDR extension instead.
    
    Author: Joao Xavier
    Source: https://gerrit.wikimedia.org/r/plugins/gitiles/mediawiki/extensions/WikibaseLexeme/+/c0d0a6f9559c115ea06e5f10bcdaa440aa5b2f3d/i18n/language-names/io.json
    Bug: T352922
    Change-Id: I3b2aa37f213265404d5db2f740e8eea2969f1b95

---
+++ LocalNames/LocalNamesIo.php
@@ -0,0 +1,11 @@
+<?php
+
+/** @phpcs-require-sorted-array */
+$languageNames = [
+	'nrf-gg' => 'Guernésiais',
+	'nrf-je' => 'Jèrriais',
+	'rm-rumgr' => 'Rumantsch Grischun',
+	'rm-surmiran' => 'Surmiran',
+	'rm-sursilv' => 'Sursilvan',
+	'rm-vallader' => 'Vallader',
+];
