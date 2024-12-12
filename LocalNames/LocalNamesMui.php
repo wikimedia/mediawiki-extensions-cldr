5561c57640ca777d1a14d323fb4414d7b0e1c91e
Arthur Taylor <arthur.taylor@wikimedia.de>
2024-12-12 13:25:49 +0100

    Import language names for mui from WikibaseLexeme
    The WikibaseLexeme repository contains some language names which have
    been translated via Translatewiki. Adding them here will make the names
    available elsewhere in MediaWiki and will stop the names from being
    lost when it is updated to fetch names from the CLDR extension instead.
    
    Author: Es Krim 5 Juta Rasa
    Source: https://gerrit.wikimedia.org/r/plugins/gitiles/mediawiki/extensions/WikibaseLexeme/+/c0d0a6f9559c115ea06e5f10bcdaa440aa5b2f3d/i18n/language-names/mui.json
    Bug: T352922
    Change-Id: I7cf7d4b8c1d5069882ba81a2a7bdc4d25d7bb6c3

---
+++ LocalNames/LocalNamesMui.php
@@ -0,0 +1,12 @@
+<?php
+
+/** @phpcs-require-sorted-array */
+$languageNames = [
+	'de-1901' => 'Jerman (ortografi tradisional)',
+	'ja-hira' => 'Jepang (Urup Hiragana)',
+	'ja-hrkt' => 'Jepang (Urup Kana)',
+	'mis' => 'Baso idak didukung',
+	'sat-latn' => 'Santali (aksara Latin)',
+	'sux-latn' => 'Sumeria (aksara Sumeria)',
+	'sux-xsux' => 'Sumeria (aksara Sumeria)',
+];
