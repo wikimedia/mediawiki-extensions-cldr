7c6667ab971a1e067af709704d5ef825b98d51e0
Arthur Taylor <arthur.taylor@wikimedia.de>
2024-12-12 10:59:48 +0100

    Import language names for eo from WikibaseLexeme
    The WikibaseLexeme repository contains some language names which have
    been translated via Translatewiki. Adding them here will make the names
    available elsewhere in MediaWiki and will stop the names from being
    lost when it is updated to fetch names from the CLDR extension instead.
    
    Author: Javiero
    Author: Robin van der Vliet
    Source: https://gerrit.wikimedia.org/r/plugins/gitiles/mediawiki/extensions/WikibaseLexeme/+/c0d0a6f9559c115ea06e5f10bcdaa440aa5b2f3d/i18n/language-names/eo.json
    Bug: T352922
    Change-Id: I764ef22235d3d8ec604c8fca8825d20d217551ed

---
+++ LocalNames/LocalNamesEo.php
@@ -0,0 +1,15 @@
+<?php
+
+/** @phpcs-require-sorted-array */
+$languageNames = [
+	'bfi' => 'brita signolingvo',
+	'eo-hsistemo' => 'Esperanto (H-sistemo)',
+	'eo-xsistemo' => 'Esperanto (X-sistemo)',
+	'frm' => 'Mezfranca',
+	'fro' => 'Malnovfranca',
+	'gsg' => 'germana signolingvo',
+	'mis' => 'alia',
+	'non' => 'norena',
+	'tlh-latn' => 'klingona en la latina skribsistemo',
+	'tlh-piqd' => 'klingona en la klingona skribsistemo',
+];
