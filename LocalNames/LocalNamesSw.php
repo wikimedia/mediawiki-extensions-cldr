4853cccf702f48da38778837c3bb70c20cef62e9
Arthur Taylor <arthur.taylor@wikimedia.de>
2024-12-12 11:55:02 +0100

    Import language names for sw from WikibaseLexeme
    The WikibaseLexeme repository contains some language names which have
    been translated via Translatewiki. Adding them here will make the names
    available elsewhere in MediaWiki and will stop the names from being
    lost when it is updated to fetch names from the CLDR extension instead.
    
    Author: Edwingudfriend
    Author: Robin van der Vliet
    Source: https://gerrit.wikimedia.org/r/plugins/gitiles/mediawiki/extensions/WikibaseLexeme/+/c0d0a6f9559c115ea06e5f10bcdaa440aa5b2f3d/i18n/language-names/sw.json
    Bug: T352922
    Change-Id: I80b6909d701e25644db6ab6b75b26c5f63ca1906

---
+++ LocalNames/LocalNamesSw.php
@@ -0,0 +1,12 @@
+<?php
+
+/** @phpcs-require-sorted-array */
+$languageNames = [
+	'de-1901' => 'Kijerumani (kabla ya mageuzi ya 1996)',
+	'eo-hsistemo' => 'Kiesperanto (h-system)',
+	'eo-xsistemo' => 'Kiesperanto (x-system)',
+	'ja-hira' => 'Kijapani imeandikwa kwa hiragana',
+	'ja-hrkt' => 'Kijapani iliyoandikwa kwa hiragana na katakana',
+	'ja-kana' => 'Kijapani imeandikwa kwa hiragana',
+	'lij-mc' => 'Monegasque',
+];
