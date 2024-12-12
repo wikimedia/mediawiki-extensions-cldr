3f78b8bc745905ccbc985eb00b1e7dc7b894efb5
Arthur Taylor <arthur.taylor@wikimedia.de>
2024-12-12 11:57:31 +0100

    Import language names for tk from WikibaseLexeme
    The WikibaseLexeme repository contains some language names which have
    been translated via Translatewiki. Adding them here will make the names
    available elsewhere in MediaWiki and will stop the names from being
    lost when it is updated to fetch names from the CLDR extension instead.
    
    Author: TayfunEt.
    Author: Zolgoyo
    Source: https://gerrit.wikimedia.org/r/plugins/gitiles/mediawiki/extensions/WikibaseLexeme/+/c0d0a6f9559c115ea06e5f10bcdaa440aa5b2f3d/i18n/language-names/tk.json
    Bug: T352922
    Change-Id: I0db11439722eb478d24dad5e3901d178a2bdd927

---
+++ LocalNames/LocalNamesTk.php
@@ -0,0 +1,18 @@
+<?php
+
+/** @phpcs-require-sorted-array */
+$languageNames = [
+	'az-cyrl' => 'Kiril ýazuwynda azerbaýjan',
+	'ccp-beng' => 'Çakma, Bengali ýazuwynda',
+	'ctg' => 'Çitagon',
+	'de-1901' => 'Nemes (1996-njy ýyldaky reforma çenli)',
+	'enm' => 'Orta Iňlis',
+	'eo-hsistemo' => 'Esperanto (h-ulgam)',
+	'eo-xsistemo' => 'Esperanto (x-ulgam)',
+	'frm' => 'Orta fransuz',
+	'fro' => 'Köne fransuz',
+	'ha-arab' => 'Arap ýazuwynda Hausa',
+	'ja-hira' => 'Hiragana ýazuwynda ýapon',
+	'ja-hrkt' => 'Hiragana we Katakana ýazuwynda ýapon',
+	'ja-kana' => 'Katakana ýazuwynda ýapon',
+];
