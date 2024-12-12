1a891b82ddb5c35574a0e8507bdd95701d6064d1
Arthur Taylor <arthur.taylor@wikimedia.de>
2024-12-12 10:48:04 +0100

    Import language names for br from WikibaseLexeme
    The WikibaseLexeme repository contains some language names which have
    been translated via Translatewiki. Adding them here will make the names
    available elsewhere in MediaWiki and will stop the names from being
    lost when it is updated to fetch names from the CLDR extension instead.
    
    Author: Adriendelucca
    Author: Huñvreüs
    Source: https://gerrit.wikimedia.org/r/plugins/gitiles/mediawiki/extensions/WikibaseLexeme/+/c0d0a6f9559c115ea06e5f10bcdaa440aa5b2f3d/i18n/language-names/br.json
    Bug: T352922
    Change-Id: I0e6250a38386bbb209d6f7981a0421765acf17d4

---
+++ LocalNames/LocalNamesBr.php
@@ -0,0 +1,20 @@
+<?php
+
+/** @phpcs-require-sorted-array */
+$languageNames = [
+	'bfi' => 'yezh ar sinoù Breizh-Veur',
+	'bzs' => 'yezh ar sinoù Brazil',
+	'enm' => 'krennsaozneg',
+	'gsg' => 'yezh ar sinoù Alamagn',
+	'mis' => 'yezh diskor',
+	'nn-hognorsk' => 'høgnorsk (norvegeg uhel)',
+	'non-runr' => 'norseg skrivet gant runoù',
+	'nrf-gg' => 'gwernenezeg',
+	'nrf-je' => 'jerzenezeg',
+	'obt' => 'henvrezhoneg',
+	'pks' => 'yezh ar sinoù Pakistan',
+	'rm-rumgr' => 'romañcheg Grison',
+	'sia' => 'samieg Akkala',
+	'sjk' => 'samieg Kemi',
+	'xbm' => 'krennvrezhoneg',
+];
