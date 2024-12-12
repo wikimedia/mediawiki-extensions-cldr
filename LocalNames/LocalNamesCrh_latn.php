4329ae9e63e0451e3530f3ac887af858780d79cc
Arthur Taylor <arthur.taylor@wikimedia.de>
2024-12-12 13:21:13 +0100

    Import language names for crh-latn from WikibaseLexeme
    The WikibaseLexeme repository contains some language names which have
    been translated via Translatewiki. Adding them here will make the names
    available elsewhere in MediaWiki and will stop the names from being
    lost when it is updated to fetch names from the CLDR extension instead.
    
    Author: Don Alessandro
    Author: TayfunEt.
    Author: Zolgoyo
    Source: https://gerrit.wikimedia.org/r/plugins/gitiles/mediawiki/extensions/WikibaseLexeme/+/c0d0a6f9559c115ea06e5f10bcdaa440aa5b2f3d/i18n/language-names/crh-latn.json
    Bug: T352922
    Change-Id: I4edeec8ec242b40dbabc670cc3f8836d798538ee

---
+++ LocalNames/LocalNamesCrh_latn.php
@@ -0,0 +1,14 @@
+<?php
+
+/** @phpcs-require-sorted-array */
+$languageNames = [
+	'az-cyrl' => 'Azerbaycanca (kiril urufatı)',
+	'bas' => 'Basaa',
+	'enm' => 'Orta İnglizce',
+	'frm' => 'Orta Frenkçe',
+	'fro' => 'Eski Frenkçe',
+	'ja-hira' => 'Yaponca (hiragana urufatı)',
+	'ja-hrkt' => 'Yaponca (kana urufatı)',
+	'ja-kana' => 'Yaponca (katakana urufatı)',
+	'lad-hebr' => 'Ladinoca (ibrani urufatı)',
+];
