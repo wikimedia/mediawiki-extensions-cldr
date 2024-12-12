473d82c6a2d3cbcfa434bbe8cbe02be648ff8935
Arthur Taylor <arthur.taylor@wikimedia.de>
2024-12-12 11:25:45 +0100

    Import language names for ko from WikibaseLexeme
    The WikibaseLexeme repository contains some language names which have
    been translated via Translatewiki. Adding them here will make the names
    available elsewhere in MediaWiki and will stop the names from being
    lost when it is updated to fetch names from the CLDR extension instead.
    
    Author: Ykhwong
    Source: https://gerrit.wikimedia.org/r/plugins/gitiles/mediawiki/extensions/WikibaseLexeme/+/c0d0a6f9559c115ea06e5f10bcdaa440aa5b2f3d/i18n/language-names/ko.json
    Bug: T352922
    Change-Id: I1f454fe1b9710c014fb2b6f0fc9217bde5dedfcc

---
+++ LocalNames/LocalNamesKo.php
@@ -0,0 +1,11 @@
+<?php
+
+/** @phpcs-require-sorted-array */
+$languageNames = [
+	'bzs' => '브라질어 수화',
+	'de-1901' => '독일어 (1996년 개정 이전)',
+	'ja-hira' => '일본어 (히라가나 문자)',
+	'ja-hrkt' => '일본어 (가나 문자)',
+	'ja-kana' => '일본어 (가타카나 문자)',
+	'mis' => '지원하지 않는 언어',
+];
